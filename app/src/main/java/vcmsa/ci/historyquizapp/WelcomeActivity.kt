a14b8e851e72623cfa994eb8da6505cfc8a4c27f
ST10491892 <ST10491892@vcconnect.edu.za>
2025-05-20 14:35:09 +0200

    Initial commit

---
+++ app/src/main/java/vcmsa/ci/historyquizapp/WelcomeActivity.kt
@@ -0,0 +1,26 @@
+package vcmsa.ci.historyquizapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Set the layout for this screen
+        setContentView(R.layout.activity_welcome)
+
+        // Get the Start button using its ID
+        val startButton =
+            findViewById<Button>(R.id.startButton)
+
+        // Set what happens when the Start button is clicked
+        startButton.setOnClickListener {
+            // Create a new screen (intent) to move to the QuizActivity
+            val intent = Intent (this, QuizActivity::class.java)
+            // Start the QuizActivity screen
+            startActivity(intent)
+        }
+    }
+}
